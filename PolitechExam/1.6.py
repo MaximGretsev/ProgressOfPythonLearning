1e063a34e35e4cce3492eaf73942eb8f47a6cb2b
MaximGretsev <98987566+MaximGretsev@users.noreply.github.com>
2022-03-11 22:29:33 +0300

    Add files via upload

---
+++ PolitechExam/1.6.py
@@ -0,0 +1,6 @@
+int_a = int(input('Enter number: '))
+
+if (int_a >= 1000 and int_a != 4999 and int_a <= 9999):
+    print('Является 4-х значным и не равно 4999')
+else:
+    print('Не является 4-х значным или равно 4999')
